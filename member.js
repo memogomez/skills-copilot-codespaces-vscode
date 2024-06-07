df931d94a1e115fc677f9366e5c8daf8671e2b6b
Guillermo Gómez Maya <guillermogomezmaya@gmail.com>
2024-06-07 15:11:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillMember() {
+    var member = {
+        name: 'John Doe',
+        age: 32,
+        skills: {
+            main: 'JavaScript',
+            secondary: 'React',
+            experience: 'Intermediate'
+        }
+    };
+
+    return member;
+}
